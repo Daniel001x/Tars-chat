ef6b81f8869dfd83d55605d856b4f0684512e8a7
enginron <danishmirzamirza001@gmail.com>
2026-02-22 17:05:22 +0530

    the middleware has been updatde to the new version

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
-  const { userId } = await auth();
-
-  if (!userId && !isPublicRoute(request)) {
-    return Response.redirect(new URL("/sign-in", request.url));
+  if (!isPublicRoute(request)) {
+    await auth.protect();
   }
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
