530d4efbeccf4da3cc992c04887a74adb37903dd
enginron <danishmirzamirza001@gmail.com>
2026-02-26 23:38:31 +0530

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "img.clerk.com",
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
       },
       {
-        protocol: "https",
-        hostname: "api.dicebear.com",
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.clerk.dev',
       },
     ],
   },
